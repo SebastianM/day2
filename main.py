b195d6cce07fd492f4f7fa61cc7d3171deddb2b4
Sebastian Matthews <109438074+SebastianM@users.noreply.github.com>
2022-07-18 19:03:34 +0000

    tells you if the number is big or small

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ import random
 
 num = random.randint(0, 1000)
 
-print(num)
+if(num < 500):
+  print(num)
+  print("this is a small number")
+if(num > 500):
+  print(num)
+  print("this is a big number")
